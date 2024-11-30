c8442d84879430304b7cbf6993dda7b64687fe59
Georges <georgeslecomteone@gmail.com>
2024-11-30 17:30:22 +0100

    ajout du pause menu manager dans les prefab

--- Assets/Sripts/PauseMenu.cs
+++ Assets/Sripts/PauseMenu.cs
@@ -6,14 +6,40 @@ using UnityEngine.SceneManagement;
 public class PauseMenu : MonoBehaviour
 {
     [SerializeField] private GameObject pausePanel;
-    
+
+    private bool gameIsPaused = false;
+
+    void Start()
+    {
+        pausePanel.SetActive(false);
+    }
+
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Escape)) {
+            if(gameIsPaused) {
+                ResumeGame();
+            }
+            else {
+                PauseGame();
+            }
+        }
+    }
+
+    private void PauseGame() {
+        pausePanel.SetActive(true);
+        gameIsPaused = true;
+    }
+
     public void ResumeGame() 
     {
         pausePanel.SetActive(false);
+        gameIsPaused = false;
     }
 
     public void MainMenu()
     {
         SceneManager.LoadScene("Menu");
+        gameIsPaused = false;
     }
 }
